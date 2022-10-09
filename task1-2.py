75be942c162be04bae7527cf7f61f7c8c68b4880
Pavel Korobov <korobovpvl@gmail.com>
2022-10-09 03:48:38 +0300

    Initial commit

---
+++ task1-2.py
@@ -0,0 +1,50 @@
+def get_shop_list_from_dishes(dishes, person_count):
+    ingridients = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingridient in cook_book[dish]:
+                if ingridient["ingridient_name"] in ingridients:
+                    ingridients[ingridient["ingridient_name"]]["quanity"] += ingridient["quanity"] * person_count  
+                else:                                                                                             
+                    ingridients[ingridient["ingridient_name"]] = {                                                
+                                                                 "measure": ingridient["measure"].strip(),                
+                                                                 "quanity": ingridient["quanity"] * person_count                 
+                                                                 }                                                                                 
+    # ingridient in cook_book[dish] - итерация по списку словарей,
+    # содержащих информацию об ингридинте блюда
+    # ingridients[ingridient["ingridient_name"]]["quanity"] - обращение
+    # к внутреннему словарю, вложенному в словарь созданный внутри функции
+    # по тому же значению, что уже лежит в словаре ingridient под ключом
+    # "ingridient_name"
+    # ingridients[ingridient["ingridient_name"]]["quanity"] += ingridient["quanity"] * person_count
+    # это прибавление количетсва ингридинта, необходимого для блюда,
+    # умноженного на количество людей person_count, к уже имеющемуся количетву,
+    # которое и лежит во "внутреннем словаре"
+    # ingridients == {'Картофель': {'measure': кг, 'quanity': 2}}
+    #                                                         ^
+    #                                                 Воооооооот сюда
+        else:
+            print("Искомого блюда нет в книге рецептов")
+            return {}
+    return ingridients
+
+cook_book = {}
+with open("recipes.txt", "r", encoding="utf-8") as recipebook:
+    for line in recipebook:
+        dish_name = line.strip()
+        cook_book[dish_name] = []
+        ingridient_amount = recipebook.readline()
+        for ingridient in range(int(ingridient_amount)):
+            ingridient = recipebook.readline()
+            listed_ingredient = ingridient.split(" | ")
+
+            ingridient_name = listed_ingredient[0]
+            quanity = int(listed_ingredient[1])
+            measure = listed_ingredient[2]
+
+            cook_book[dish_name] += [{"ingridient_name": ingridient_name, 
+                                    "quanity": quanity, 
+                                    "measure": measure}]
+        blank = recipebook.readline()
+# print(cook_book)
+print(get_shop_list_from_dishes(["Омлет"], 4))
